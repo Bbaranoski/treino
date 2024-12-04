453410f70c6621caa9bbac3451e02268d3af7465
Bbaranoski <130513920+Bbaranoski@users.noreply.github.com>
2024-12-04 13:56:33 -0300

    change the way that is display the teste variable from useState

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import { useState, useEffect } from 'react'
 function App() {
   const [teste, setTeste] = useState(0)
  
-  return <button onClick={() => {
-    setTeste(teste => teste + 1)
-  }}>{teste}</button>
+  return <div>
+    
+      <button onClick={() => {
+        setTeste(teste => teste + 1)
+      }}>click</button>
+  
+      <p>{teste}</p>
+    </div>
 }
-
 export default App;
